72fc1a07558891ffe56dcf3351f6eeccfd69915b
Andreea Miculescu <andreea.miculescu@student.upt.ro>
2022-11-08 12:01:55 +0200

    Added Post and Comment entity classes

---
+++ ZeroToHero_backend/src/main/java/Entities/WishPost.java
@@ -0,0 +1,10 @@
+package Entities;
+
+import java.util.ArrayList;
+
+public class WishPost {
+    private String post_text;
+    private String username;
+    private boolean hearted;
+    private ArrayList<Comment> comments;
+}
