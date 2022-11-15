6f28d3df0d31cccc7b3f85019761379b774d60ee
Eliza Martuica <iulia.martuica@student.upt.ro>
2022-11-15 13:00:34 +0200

    changed background color

--- ZeroToHero_fe/App.js
+++ ZeroToHero_fe/App.js
@@ -48,7 +48,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#DFAEB4",
     alignItems: "center",
     justifyContent: "center",
   },
