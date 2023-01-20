356a5be88984eebbe978830ad76f05db6da4a342
Eliza Martuica <iulia.martuica@student.upt.ro>
2023-01-20 02:05:59 +0200

    mapped posts to Home page

--- ZeroToHero_fe/screens/Post.js
+++ ZeroToHero_fe/screens/Post.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Text, ImageBackground, Dimensions, Alert, TouchableOpacity, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Header2 from '../components/Header2';
 import NavBar from '../components/NavBar';
 
-export default Post = () => {
-    
+const Post = props => {
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#82667F' }}>
             <Header2 title="ZeroToHero" />
 
-            <View style={styles.container}>
-                {/* <Text>Home</Text> */}
+            
+            <View>
+
             </View>
+
 
             <NavBar title1='chevron-circle-left' title2='home' title3='user-alt'></NavBar>
         </SafeAreaView>
@@ -29,3 +30,5 @@ styles = StyleSheet.create({
         backgroundColor: 'white',
     },
 });
+
+export default Post;
